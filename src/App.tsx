210e361247290145b25b912ea4c6a8f0caad8a2c
Alex <alvkonovalov@gmail.com>
2022-08-13 18:15:48 +0300

    update create-react-app version, add typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Button from "./components/Button/Button";
+
+function App() {
+    return (
+        <div className="App">
+            Hello world!
+            <Button children={"Submit"} />
+        </div>
+    );
+}
+
+export default App;
--- src/api.js
+++ src/api.js
@@ -1,34 +1,35 @@
 export function fetchChecks() {
-  return new Promise((resolve, reject) =>
-    setTimeout(
-      () =>
-        Math.random() <= 0.8
-          ? resolve([
-              {
-                id: "aaa",
-                priority: 10,
-                description: "Face on the picture matches face on the document"
-              },
-              {
-                id: "bbb",
-                priority: 5,
-                description: "Veriff supports presented document"
-              },
-              {
-                id: "ccc",
-                priority: 7,
-                description: "Face is clearly visible"
-              },
-              {
-                id: "ddd",
-                priority: 3,
-                description: "Document data is clearly visible"
-              }
-            ])
-          : reject({ success: false }),
-      500
-    )
-  );
+    return new Promise((resolve, reject) =>
+        setTimeout(
+            () =>
+                Math.random() <= 0.8
+                    ? resolve([
+                          {
+                              id: "aaa",
+                              priority: 10,
+                              description:
+                                  "Face on the picture matches face on the document",
+                          },
+                          {
+                              id: "bbb",
+                              priority: 5,
+                              description: "Veriff supports presented document",
+                          },
+                          {
+                              id: "ccc",
+                              priority: 7,
+                              description: "Face is clearly visible",
+                          },
+                          {
+                              id: "ddd",
+                              priority: 3,
+                              description: "Document data is clearly visible",
+                          },
+                      ])
+                    : reject({ success: false }),
+            500
+        )
+    );
 }
 
 /**
@@ -37,11 +38,13 @@ export function fetchChecks() {
  * @param {string} results[].result - Result value (yes / no)
  */
 export function submitCheckResults(results) {
-  return new Promise((resolve, reject) =>
-    setTimeout(
-      () =>
-        Math.random() <= 0.8 ? resolve(results) : reject({ success: false }),
-      500
-    )
-  );
+    return new Promise((resolve, reject) =>
+        setTimeout(
+            () =>
+                Math.random() <= 0.8
+                    ? resolve(results)
+                    : reject({ success: false }),
+            500
+        )
+    );
 }
